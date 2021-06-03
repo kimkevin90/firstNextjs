acd7662e7eafe503a4827945b0f6391dd6b056a2
kimkevin90 <65535673+kimkevin90@users.noreply.github.com>
2021-06-03 22:01:56 +0900

    Update firebase.js

--- firstnextjs/firebase.js
+++ firstnextjs/firebase.js
@@ -1,11 +1,6 @@
 import firebase from "firebase";
 const firebaseConfig = {
-  apiKey: "AIzaSyDx2HnP3cuPLYt4UThx85KD4mD1mvXV-u4",
-  authDomain: "cahtapp2.firebaseapp.com",
-  projectId: "cahtapp2",
-  storageBucket: "cahtapp2.appspot.com",
-  messagingSenderId: "1089911542663",
-  appId: "1:1089911542663:web:5907ca3826bff8dd7b3922",
+
 };
 
 const app = !firebase.apps.length
